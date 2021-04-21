01a70f05cc8ccce9c450332c6569688a53df6f63
Rafael Estrada <rafaelestrada@comcast.net>
2021-04-20 22:04:51 -0700

    first commit for quiz app

---
+++ quiz-app/script.js
@@ -0,0 +1,40 @@
+const quizData = [  {
+    question: 'How old is Rafael?',
+    a: '10',
+    b: '17',
+    c: '26',
+    d: '100',
+    correct: 'd'
+
+},{
+question: 'What is the best programming language?',
+a: 'Cobol',
+b: 'Fortran',
+c: 'Javascript',
+d: 'PHP',
+correct: 'c'
+}, {
+    question: 'Who is President of US?',
+    a: 'Biden',
+    b: 'Trump',
+    c: 'Rafael',
+    d: 'Kamala',
+    correct: 'a'
+
+},{
+    question: 'What does HTML stand for?',
+    a: 'Cascading Style Sheet',
+    b: 'Hypertext Markup Language',
+    c: 'Jason Object Notation',
+    d: 'Application Programming Interface',
+    correct: 'b' 
+},{
+    question: 'What year was Javascript launched?',
+    a: '2020',
+    b: '2016',
+    c: '2000',
+    d: 'None of the above',
+    correct: 'd' 
+}
+
+]
