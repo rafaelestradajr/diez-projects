945eedd7c605a1e034160cd66f39e8ff9e2171dc
Rafael Estrada <rafaelestrada@comcast.net>
2021-04-21 22:21:35 -0700

    added questions

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -1,40 +1,68 @@
-const quizData = [  {
-    question: 'How old is Rafael?',
-    a: '10',
-    b: '17',
-    c: '26',
-    d: '100',
-    correct: 'd'
+const quizData = [
+  {
+    question: "How old is Rafael?",
+    a: "10",
+    b: "17",
+    c: "26",
+    d: "100",
+    correct: "d",
+  },
+  {
+    question: "What is the best programming language?",
+    a: "Cobol",
+    b: "Fortran",
+    c: "Javascript",
+    d: "PHP",
+    correct: "c",
+  },
+  {
+    question: "Who is President of US?",
+    a: "Biden",
+    b: "Trump",
+    c: "Rafael",
+    d: "Kamala",
+    correct: "a",
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Cascading Style Sheet",
+    b: "Hypertext Markup Language",
+    c: "Jason Object Notation",
+    d: "Application Programming Interface",
+    correct: "b",
+  },
+  {
+    question: "What year was Javascript launched?",
+    a: "2020",
+    b: "2016",
+    c: "2000",
+    d: "None of the above",
+    correct: "d",
+  },
+];
 
-},{
-question: 'What is the best programming language?',
-a: 'Cobol',
-b: 'Fortran',
-c: 'Javascript',
-d: 'PHP',
-correct: 'c'
-}, {
-    question: 'Who is President of US?',
-    a: 'Biden',
-    b: 'Trump',
-    c: 'Rafael',
-    d: 'Kamala',
-    correct: 'a'
+const quiz = document.getElementById("quiz");
+const answers = document.getElementById(".answer");
 
-},{
-    question: 'What does HTML stand for?',
-    a: 'Cascading Style Sheet',
-    b: 'Hypertext Markup Language',
-    c: 'Jason Object Notation',
-    d: 'Application Programming Interface',
-    correct: 'b' 
-},{
-    question: 'What year was Javascript launched?',
-    a: '2020',
-    b: '2016',
-    c: '2000',
-    d: 'None of the above',
-    correct: 'd' 
+const questionE1 = document.getElementById("question");
+
+const a_text = document.getElementById("a-text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  const currentQuizData = quizData[currentQuiz];
+  questionE1.innerText = currentQuizData.question;
+
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+
+ currentQuestion++;
 }
-
-]
